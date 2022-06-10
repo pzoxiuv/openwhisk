2f12eda09f0854c73213f419a92050fc69b73942
Alex Merenstein <mmerenstein@cs.stonybrook.edu>
2022-06-10 03:49:06 -0400

    Add logging to cmd.py to log the action name, params, and pod uid

--- test-actions/cmd.py
+++ test-actions/cmd.py
@@ -4,7 +4,27 @@ import json
 import sys
 import os
 
+def get_uid():
+    try:
+        with open('/proc/1/environ') as f:
+            e = f.read()
+        uid = [x for x in e.split('\x00') if 'POD_UID' in x]
+        uid = uid[0].split('=')[1]
+        return uid
+    except Exception as e:
+        print(e)
+        return 'unknown'
+
+def log_action_pod(params):
+    o = {'params': params, 'pod-uid': get_uid(), 'action': os.environ.get('__OW_ACTION_NAME', 'unknown')}
+    print(f'Logging action-pod info {json.dumps(o)}')
+    with open('/var/log/f3/action-pods', 'a') as f:
+        json.dump(o, f)
+        f.write('\n')
+
 def main(dict):
+    log_action_pod(dict)
+
     if 'command' in dict:
         cmd = dict['command']
     else:
@@ -19,4 +39,5 @@ def main(dict):
     return {'stdout': proc.stdout.decode('ascii'), 'stderr': proc.stderr.decode('ascii')}
 
 if __name__ == '__main__':
+    #log_action_pod(sys.argv[1])
     print(main(json.loads(sys.argv[1])))
